6567b93a9d3cfe99da97255716b3b01e86145246
Eduardo Calvo <educlopez93@gmail.com>
2026-06-04 19:28:20 +0200

    release: v1.8.1
    Bump STATUSLINE_VERSION to 1.8.1 in all scripts.

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ set -euo pipefail
 #   ./install.sh --force               # repair/overwrite an existing install
 #   ./install.sh --help
 
-STATUSLINE_VERSION="1.8.0"
+STATUSLINE_VERSION="1.8.1"
 
 SCRIPT_NAME="statusline-command.sh"
 
